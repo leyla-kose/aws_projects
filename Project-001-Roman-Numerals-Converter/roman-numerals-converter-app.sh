c01edab7f2cf82d1d5cabaa2bcd8cb7a2abaae6a
leylakose <leylaboun@gmail.com>
2022-02-20 00:05:28 -0800

    developer name changed in project

--- Project-001-Roman-Numerals-Converter/roman-numerals-converter-app.sh
+++ Project-001-Roman-Numerals-Converter/roman-numerals-converter-app.sh
@@ -2,16 +2,14 @@
 
 yum update -y
 python3 --version
-sudo yum install flask
 sudo pip3 install flask
-python3 --version
 sudo yum install git -y
-
 mkdir flask
 cd flask
 mkdir templates
 cd templates
-sudo wget
-
-
+sudo wget https://raw.githubusercontent.com/leyla-kose/aws_projects/main/Project-001-Roman-Numerals-Converter/templates/index.html
+sudo wget https://raw.githubusercontent.com/leyla-kose/aws_projects/main/Project-001-Roman-Numerals-Converter/templates/result.html
+cd ..
+sudo wget https://raw.githubusercontent.com/leyla-kose/aws_projects/main/Project-001-Roman-Numerals-Converter/app.py
 sudo python3 app.py
