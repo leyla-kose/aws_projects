75eabacac90a2d08f33c63afe68a0e7969e2b963
leylakose <leylaboun@gmail.com>
2022-03-25 12:04:34 -0700

    endpoint added

--- Project-503-Capstone-Project-Blog-Page-App-(Django)-on-AWS-Environment/src/cblog/settings.py
+++ Project-503-Capstone-Project-Blog-Page-App-(Django)-on-AWS-Environment/src/cblog/settings.py
@@ -85,10 +85,10 @@ WSGI_APPLICATION = 'cblog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'projectdatabase', # database name in RDS is written here
+        'NAME': 'projectdatabase1', # database name in RDS is written here
         'USER': 'admin', # database master username in RDS is written here
         'PASSWORD': config('PASSWORD'),
-        'HOST': 'projectdatabase.con76kc9s0ti.us-east-1.rds.amazonaws.com',  # database endpoint is written here
+        'HOST': 'database-1.con76kc9s0ti.us-east-1.rds.amazonaws.com',  # database endpoint is written here
         'PORT': '3306' # database port is written here
     }
 }
